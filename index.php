9561f314a53a429bfa6a7e5684f1ad5ee8aaca51
GiadaTonioni <giada.tonioni@gmail.com>
2023-07-12 16:48:57 +0200

    wip mail

--- index.php
+++ index.php
@@ -1,6 +1,30 @@
 <?php
 require_once __DIR__.'/Models/SistemiComunicazione.php';
+require_once __DIR__.'/Models/Email.php';
 
+//Sistemi Comunicazione
 $comm = new SistemiComunicazione('Giada', 'Sofia', 'Concerto', 'Biglietti presi');
 var_dump($comm);
+
+echo "Mittente: ".$comm->getMittente()."<br/>";
+echo "Destinatario: ".$comm->getDestinatario()."<br/>";
+echo "Oggetto: ".$comm->getOggetto()."<br/>";
+echo "Contenuto: ".$comm->getContenuto()."<br/>";
+echo "Stato invio: ".$comm->Invio()."<br/>";
+
+//EMAIL
+$email = new Email('Ticketone', 'Giada', 'biglietti', 'scarica i tuoi biglietti', true);
+var_dump($email);
+
+echo "Mittente: ".$email->getMittente()."<br/>";
+echo "Destinatario: ".$email->getDestinatario()."<br/>";
+echo "Oggetto: ".$email->getOggetto()."<br/>";
+echo "Contenuto: ".$email->getContenuto()."<br/>";
+echo "Stato invio: ".$email->invio()."<br/>";
+//echo "stampa: ".$comm->stampa()."<br/>";
+//echo "inoltro: ".$comm->inoltro()."<br/>";
+
+
+
+
 ?>
